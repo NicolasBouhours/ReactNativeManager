2e18bf06373bbc1195db5c617306f2f2038e2b7f
NicolasBouhours <nico53960@hotmail.fr>
2017-02-06 20:31:00 +0100

    Adding auth with firebase

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import { View, Text } from 'react-native'
+import { createStore, applyMiddleware } from 'redux'
 import firebase from 'firebase'
+import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
   componentWillMount() {
@@ -19,13 +20,11 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={store}>
+        <LoginForm />
       </Provider>
     )
   }
